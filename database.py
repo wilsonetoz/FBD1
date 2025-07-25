13a3bdaf31671747a874de9ecdc85588b08cc901
wilsonetoz <82057984+wilsonetoz@users.noreply.github.com>
2025-07-24 22:38:26 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import psycopg2
 from psycopg2 import Error
+from fastapi import HTTPException
 
 def get_db_connection(user, password, db_name="ecomerce", host="localhost", port="5432"):
-    """Establishes and returns a database connection."""
     conn = None
     try:
         conn = psycopg2.connect(
@@ -16,3 +16,12 @@ def get_db_connection(user, password, db_name="ecomerce", host="localhost", port
     except Error as e:
         print(f"Error connecting to PostgreSQL database: {e}")
         return None
+
+def get_db_session(user: str, password: str):
+    conn = get_db_connection(user=user, password=password)
+    if not conn:
+        raise HTTPException(status_code=500, detail="Não foi possível conectar ao banco de dados.")
+    try:
+        yield conn
+    finally:
+        conn.close()
